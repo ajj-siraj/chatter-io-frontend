e99c50ff2e086ef36293fef8286e1b1d155a7d23
Sirag Ahmed <ajj.siraj@gmail.com>
2021-06-09 09:21:02 +0200

    websockets basic working integration and basic message validation

--- src/components/ChatroomView.js
+++ src/components/ChatroomView.js
@@ -1,16 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import MessageBox from "./MessageBox";
+import { w3cwebsocket } from "websocket";
+
+let client = new w3cwebsocket("ws://localhost:3002/");
+
+client.onerror = function (err) {
+  console.error("Websocket Connection Error", err);
+};
+
+client.onopen = function () {
+  console.log("WebSocket Client Connected");
+};
+
+client.onclose = function () {
+  console.warn("Client Closed");
+};
 
 function ChatroomView({ match }) {
+  let [messages, addMessage] = useState([]);
+  let [msgError, setError] = useState(false);
+
+  client.onmessage = function (e) {
+    addMessage([...messages, e.data]);
+  };
+
+  const validateMsg = (message) => {
+    const regex = /(\n+)/g;
+    if (message.length < 1 || regex.test(message)) return false;
+    else return true;
+  };
+
+  const sendMessage = (msg) => {
+    validateMsg(msg) && client.readyState === client.OPEN && client.send(msg);
+  };
+
   return (
     <>
       <Container fluid className="chatroom-container">
         <Row className="justify-content-center mt-3 mb-0">
           <Col md="8">
             <div className="chatroom-area">
-              <div className="chat-messages-area">This is the area for the chat</div>
-              <MessageBox />
+              <div className="chat-messages-area">
+                {messages.map((msg, idx) => (
+                  <div key={`msg-${idx}`}>{msg}</div>
+                ))}
+              </div>
+              <MessageBox room={match.params.chatroomName} sendMessage={sendMessage} />
             </div>
           </Col>
           <Col md="4">
--- src/components/MessageBox.js
+++ src/components/MessageBox.js
@@ -1,11 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
 
-function MessageBox() {
+function MessageBox({ sendMessage }) {
+  let [input, updateInput] = useState();
+  let inpRef = useRef(null);
+
+  
+
+  const handleClick = (message) => {
+    
+    sendMessage(message);
+    inpRef.value = "";
+  }
+
+  const handleKeyPress = (event, message) => {
+    if(event.key === "Enter") {
+      event.preventDefault();
+      handleClick(message);
+    }
+  };
+
   return (
     <div className="message-box-container">
-      <input type="textarea" className="message-box" />
-      <Button className="form-button-send">Send</Button>
+      <textarea className="message-box" ref={(el) => inpRef=el} onKeyPress={(e) => handleKeyPress(e, inpRef.value)}/>
+      <Button className="form-button-send" onClick={() => handleClick(inpRef.value)}>
+        Send
+      </Button>
     </div>
   );
 }
