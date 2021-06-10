f387dc1a45ce6c44b5fbfc4f42b3781ae7498797
Sirag Ahmed <ajj.siraj@gmail.com>
2021-06-10 19:48:43 +0200

    add 'readmore' functionality for long messages

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -1,10 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import { Ctx } from "../Context";
 
 function MessageBubble({ children }) {
   let user = useContext(Ctx).state.username;
   let owner = children.owner;
+  let [message, setMessage] = useState("");
+  let [messageRemainder, setMessageRemainder] = useState("");
+  let [readMore, setReadMore] = useState(false);
+  let [readMoreClicked, setReadMoreClicked] = useState(false);
+
+  useEffect(() => {
+    console.log(readMore, readMoreClicked);
+    if (
+      !readMore &&
+      children.message &&
+      children.type === "normal" &&
+      children.message.length > 500
+    ) {
+      setMessage(children.message.slice(0, 500) + "...");
+      setMessageRemainder(children.message.slice(500));
+      setReadMore(true);
+    }
+  }, [children]);
+
+  const handleReadMore = () => {
+    setMessage(message + messageRemainder);
+    setReadMoreClicked(true);
+  };
 
   return (
     <>
@@ -15,7 +38,12 @@ function MessageBubble({ children }) {
               <Col md="6" className="m-0 p-0">
                 <div className={`message-bubble message-bubble-owner`}>
                   <div className="message-bubble-username">{children.owner}</div>
-                  {children.message}
+                  {message}
+                  {!readMoreClicked ? (
+                    <div className="readmore" onClick={handleReadMore}>
+                      Read More
+                    </div>
+                  ) : null}
                 </div>
               </Col>
             </Row>
@@ -24,7 +52,12 @@ function MessageBubble({ children }) {
               <Col md="6" className="m-0 p-0">
                 <div className={`message-bubble message-bubble-other`}>
                   <div className="message-bubble-username">{children.owner}</div>
-                  {children.message}
+                  {message}
+                  {!readMoreClicked ? (
+                    <div className="readmore" onClick={handleReadMore}>
+                      Read More
+                    </div>
+                  ) : null}
                 </div>
               </Col>
             </Row>
