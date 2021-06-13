f0a25a79fa50a394d387620baaaa6826c86dcb43
Sirag Ahmed <ajj.siraj@gmail.com>
2021-06-13 10:05:24 +0200

    message bubble animation & color changes

--- src/components/MessageBubble.js
+++ src/components/MessageBubble.js
@@ -1,6 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useRef } from "react";
 import { Row, Col } from "react-bootstrap";
 import { Ctx } from "../Context";
+import gsap from "gsap";
+import CustomEase from "gsap/CustomEase";
+
+gsap.registerPlugin(CustomEase);
 
 function MessageBubble({ children }) {
   let user = useContext(Ctx).state.username;
@@ -10,7 +14,25 @@ function MessageBubble({ children }) {
   let [messageRemainder, setMessageRemainder] = useState("");
   let [readMore, setReadMore] = useState(false);
   let [readMoreClicked, setReadMoreClicked] = useState(false);
+  let msgBubble = useRef(null);
+  const customEase = CustomEase.create(
+    "custom",
+    "M0,0 C0.14,0 0.242,0.438 0.272,0.561 0.313,0.728 0.434,1.129 0.442,1.166 0.45,1.151 0.609,0.986 0.65,0.924 0.705,0.839 0.83,0.862 0.862,0.9 0.919,0.968 0.893,0.983 0.9,1 0.968,1 0.916,1 0.94,1 0.974,1 0.931,1 0.96,1 0.981,1 0.967,1 0.982,1 0.997,1 1,1 1,1 "
+  );
+  //Animation on component mount
+  let tl = gsap.timeline({ transformOrigin: "right" });
+  useEffect(() => {
+    tl.from(msgBubble, {
+      scale: 0.8,
+      opacity: 0,
+      duration: 0.5,
+      ease: customEase,
+    });
+    // .from(msgBubble, {scaleX: 0.5, duration: 1})
+    console.log(msgBubble);
+  }, []);
 
+  //Determine if message is too long:
   useEffect(() => {
     if (
       !readMore &&
@@ -47,7 +69,10 @@ function MessageBubble({ children }) {
       {children.type === "normal" ? (
         <>
           {owner === user ? (
-            <Row className="justify-content-end m-0 p-0">
+            <Row
+              className="justify-content-end m-0 p-0 message-bubble-container"
+              ref={(el) => (msgBubble = el)}
+            >
               <Col md="6" className="m-0 p-0">
                 <div className={`message-bubble message-bubble-owner`}>
                   <div className="message-bubble-username">{children.owner}</div>
@@ -61,7 +86,7 @@ function MessageBubble({ children }) {
               </Col>
             </Row>
           ) : (
-            <Row className="justify-content-start m-0 p-0">
+            <Row className="justify-content-start m-0 p-0" ref={(el) => (msgBubble = el)}>
               <Col md="6" className="m-0 p-0">
                 <div className={`message-bubble message-bubble-other`}>
                   <div className="message-bubble-username">{children.owner}</div>
